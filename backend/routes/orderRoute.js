7b10a927f54c61fd3b4012dc4623ad68b7689d0e
Theophas <afomassetheophas@gmail.com>
2024-08-02 13:46:25 +0100

    commit

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,13 +1,13 @@
-import express from "express";
-import authMiddleware from "../middleware/auth.js";
-import { placeOrder } from "../controllers/orderController.js";
+// import express from "express";
+// import authMiddleware from "../middleware/auth.js";
+// import { placeOrder } from "../controllers/orderController.js";
 
-const orderRouter = express.Router();
-
-
-orderRouter.post("/place", authMiddleware, placeOrder);
+// const orderRouter = express.Router();
 
 
+// orderRouter.post("/place", authMiddleware, placeOrder);
 
 
-export default orderRouter;
+
+
+// export default orderRouter;
